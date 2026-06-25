8fd12518c1214bf74468414af9fde277fba95c04
TomP22 <to.penap@duocuc.cl>
2026-06-24 23:24:11 -0400

    chore: separar configuraciones en perfil de desarrollo y conexion a Eureka

--- src/main/java/cl/municipalidad/inventario/InventarioApplication.java
+++ src/main/java/cl/municipalidad/inventario/InventarioApplication.java
@@ -2,8 +2,11 @@ package cl.municipalidad.inventario;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+
 public class InventarioApplication {
 
 	public static void main(String[] args) {
